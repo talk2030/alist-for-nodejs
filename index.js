076c9aae9fa65ea9b117fb2a325b22c776cc2341
muxinxy <zeng5360@gmail.com>
2023-11-27 13:15:33 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
 const { exec } = require('child_process');
+const https = require('https');
 const fs = require('fs');
 
-// 寻找当前目录下的 tar.gz 文件
-const tarFile = fs.readdirSync(process.cwd()).find(file => file.endsWith('.tar.gz'));
+// 下载 alist 压缩包
+const downloadUrl = 'https://github.com/alist-org/alist/releases/download/latest/alist-linux-amd64.tar.gz';
+const tarFile = 'alist-linux-amd64.tar.gz';
 
-if (!tarFile) {
-    console.error('未找到 tar.gz 文件');
-    process.exit(1);
-}
+const file = fs.createWriteStream(tarFile);
+const request = https.get(downloadUrl, response => {
+    response.pipe(file);
+    file.on('finish', () => {
+        file.close(() => {
+            console.log('下载完成');
+            // 解压文件
+            exec(`tar -zxvf ${tarFile}`, (err, stdout, stderr) => {
+                if (err) {
+                    console.error(`解压文件时出错： ${err}`);
+                    return;
+                }
+                console.log(`解压文件成功：${stdout}`);
+                
+                // 删除压缩包
+                fs.unlink(tarFile, err => {
+                    if (err) {
+                        console.error(`删除压缩包时出错： ${err}`);
+                        return;
+                    }
+                    console.log(`压缩包删除成功`);
 
-// 解压文件
-exec(`tar -zxvf ${tarFile}`, (err, stdout, stderr) => {
-    if (err) {
-        console.error(`解压文件时出错： ${err}`);
-        return;
-    }
-    console.log(`解压文件成功：${stdout}`);
-    
-    // 授予执行权限
-    exec('chmod +x alist', (err, stdout, stderr) => {
-        if (err) {
-            console.error(`授予权限时出错： ${err}`);
-            return;
-        }
-        console.log(`授予权限成功：${stdout}`);
+                    // 授予执行权限
+                    exec('chmod +x alist', (err, stdout, stderr) => {
+                        if (err) {
+                            console.error(`授予权限时出错： ${err}`);
+                            return;
+                        }
+                        console.log(`授予权限成功：${stdout}`);
 
-        // 运行程序
-        exec('./alist server', (err, stdout, stderr) => {
-            if (err) {
-                console.error(`运行程序时出错： ${err}`);
-                return;
-            }
-            // 程序运行成功，不再输出提示
+                        // 运行程序
+                        exec('./alist server', (err, stdout, stderr) => {
+                            if (err) {
+                                console.error(`运行程序时出错： ${err}`);
+                                return;
+                            }
+                            // 程序运行成功，不再输出提示
+                        });
+                    });
+                });
+            });
         });
     });
+}).on('error', err => {
+    console.error(`下载文件时出错： ${err}`);
 });
